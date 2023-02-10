3b5536b11def04ec4d0f7cfa32a3279df495b83a
Hieu <xuanbachrtc@gmail.com>
2023-02-10 22:10:10 +0700

    feat: add new Qr code

--- src/views/bus-schedule/components/form-payment-detail/src/FormPaymentDetail.vue
+++ src/views/bus-schedule/components/form-payment-detail/src/FormPaymentDetail.vue
@@ -1,24 +1,22 @@
 <template>
-  <BasicForm title="Payment Page Detail" okText="Payment Deta" @register="register">
-  </BasicForm>
+  <BasicForm title="Payment Page Detail" okText="Payment Deta" @register="register"> </BasicForm>
 </template>
-  
-  <script lang="ts" setup>
-import { Steps, Step, TypographyTitle } from 'ant-design-vue';
 
-import { BasicForm, useForm } from '/@/components/Form';
+<script lang="ts" setup>
+  import { Steps, Step, TypographyTitle } from 'ant-design-vue';
 
-import { schema } from './form.data';
+  import { BasicForm, useForm } from '/@/components/Form';
 
-const [register, { setFieldsValue, getFieldsValue, resetFields }] = useForm({
-  schemas: schema,
-  showActionButtonGroup: false,
-});
+  import { schema } from './form.data';
 
-defineExpose({
-  setFieldsValue,
-  getFieldsValue,
-  resetFields,
-});
+  const [register, { setFieldsValue, getFieldsValue, resetFields }] = useForm({
+    schemas: schema,
+    showActionButtonGroup: false,
+  });
+
+  defineExpose({
+    setFieldsValue,
+    getFieldsValue,
+    resetFields,
+  });
 </script>
-  
--- src/views/bus-schedule/components/modal-payment-ticket-detail/src/ModalPaymentTicketDetail.vue
+++ src/views/bus-schedule/components/modal-payment-ticket-detail/src/ModalPaymentTicketDetail.vue
@@ -1,20 +1,24 @@
 <template>
   <BasicModal
     width="60%"
-    :height="1000"
     title="Payment ticket"
     okText="Pay"
     @register="register"
     @close="handleClose"
     @ok="handlePayment"
   >
-    <FormPaymentDetail ref="formTicketInfoRef" />
+    <FormPaymentDetail ref="formTicketInfoRef" v-show="!openQrcode" />
+    <div v-if="openQrcode">
+      <div class="flex justify-center">
+        <qrcode-vue value="https://example.com" :size="300" level="H" />
+      </div>
+    </div>
   </BasicModal>
 </template>
 
 <script lang="ts" setup>
   import { ref, unref } from 'vue';
-
+  import QrcodeVue from 'qrcode.vue';
   import { BasicModal, useModalInner } from '/@/components/Modal';
   import { useUserStore } from '/@/store/modules/user';
   import { FormPaymentDetail } from '../../form-payment-detail';
@@ -22,7 +26,7 @@
 
   const emit = defineEmits(['register', 'submit-success']);
   const { createMessage } = useMessage();
-
+  const openQrcode = ref(false);
   const formTicketInfoRef = ref();
 
   const [register, { closeModal }] = useModalInner((data) => {
@@ -32,12 +36,17 @@
   const userStore = useUserStore();
 
   const handlePayment = () => {
-    createMessage.success('Payment success');
-    emit('submit-success');
-    handleClose();
+    if (openQrcode.value) {
+      createMessage.success('Payment success');
+      emit('submit-success');
+      handleClose();
+    } else {
+      openQrcode.value = true;
+    }
   };
 
   const handleClose = () => {
     closeModal();
+    openQrcode.value = false;
   };
 </script>
